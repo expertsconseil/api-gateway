ad6aad97657f231e21e64d888704fc172eb569a6
maher.formations@gmail.com <maher.formations@gmail.com>
2025-09-22 02:28:03 +0200

    add Zipking dependencies in pom.xml

---
+++ src/main/java/com/expertsconseil/api_gateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,33 @@
+package com.expertsconseil.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader","MyUri")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/user/**")
+                        .uri("lb://user-service"))
+                .route(p -> p.path("/user-newUrl/**")
+                        .filters(f -> f.rewritePath("/user-newUrl/(?<segment>.*)", "/user/${segment}"))
+                        .uri("lb://user-service"))
+                .route(p -> p.path("/quiz/**")
+                        .uri("lb://quiz-service"))
+                .build();
+
+    }
+}
